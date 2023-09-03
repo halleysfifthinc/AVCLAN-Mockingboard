064fc01ac6e13a8b08abe088e84b74ff11b0a220
Allen Hill <allenofthehills@gmail.com>
2023-09-03 13:43:49 -0400

    Update LICENSE and notices in files

--- src/avclandrv.c
+++ src/avclandrv.c
@@ -1,30 +1,22 @@
 /*
-  Copyright (C) 2015 Allen Hill <allenofthehills@gmail.com>.
+                        AVCLAN-Mockingboard
+    Copyright (C) 2015 Allen Hill <allenofthehills@gmail.com>
 
-  Portions of the following source code are:
-  Copyright (C) 2006 Marcin Slonicki <marcin@softservice.com.pl>.
+    Portions of the following source code are based on code that is
+    copyright (C) 2006 Marcin Slonicki <marcin@softservice.com.pl>
 
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software Foundation,
-  Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
- -----------------------------------------------------------------------
-    this file is a part of the TOYOTA Corolla MP3 Player Project
- -----------------------------------------------------------------------
-         http://www.softservice.com.pl/corolla/avc
-
- May 28 / 2009	- version 2
-
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include <avr/io.h>
