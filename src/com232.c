6d62fc40950b388410464f737d00bf371b977506
Allen Hill <allenofthehills@gmail.com>
2023-10-23 14:13:53 -0400

    Remove copy of libc function

--- src/com232.c
+++ src/com232.c
@@ -125,23 +125,3 @@ void RS232_PrintDec2(uint8_t Data) {
     RS232_SendByte('0');
   RS232_PrintDec(Data);
 }
-
-char *itoa(int i, char b[]) {
-  char const digit[] = "0123456789";
-  char *p = b;
-  if (i < 0) {
-    *p++ = '-';
-    i *= -1;
-  }
-  int shifter = i;
-  do { // Move to where representation ends
-    ++p;
-    shifter = shifter / 10;
-  } while (shifter);
-  *p = '\0';
-  do { // Move back, inserting digits as u go
-    *--p = digit[i % 10];
-    i = i / 10;
-  } while (i);
-  return b;
-}
